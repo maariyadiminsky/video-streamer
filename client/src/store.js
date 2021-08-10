80f6e48705626166ce01f766320283bbab883aca
maariyadiminsky <mariya.github@gmail.com>
2021-08-10 13:11:48 -0600

    seperated store out into its own file

---
+++ client/src/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
+
+import reducers from "./reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
