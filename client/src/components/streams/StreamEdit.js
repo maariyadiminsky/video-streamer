9f6fddf7fc5b0f7315fdbbcef665f8cb0e9dd9dc
maariyadiminsky <mariya.github@gmail.com>
2021-08-10 14:58:39 -0600

    call getStreams on load only

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
@@ -15,7 +15,7 @@ const StreamEdit = ({ history }) => {
 
     useEffect(() => {
         if (!stream) dispatch(getStream(id));
-    }, [stream, id, dispatch]);
+    }, []);
 
     const handleOnSubmit = (formValues) => {
     /*  
